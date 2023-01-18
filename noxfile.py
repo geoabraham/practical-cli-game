ce0f7d8bdb1b16c83540748037ca33439464c03a
Gerardo Abraham <geoabraham@tuta.io>
2023-01-18 17:31:53 -0300

    chore: ignore py vulnerability  https://github.com/github/advisory-database/pull/762#issuecomment-1286776703

--- noxfile.py
+++ noxfile.py
@@ -143,7 +143,7 @@ def safety(session: Session) -> None:
     """Scan dependencies for insecure packages."""
     requirements = session.poetry.export_requirements()
     session.install("safety")
-    session.run("safety", "check", "--full-report", f"--file={requirements}")
+    session.run("safety", "check", "--full-report", "--ignore=51457", f"--file={requirements}")
 
 
 @session(python=python_versions)
