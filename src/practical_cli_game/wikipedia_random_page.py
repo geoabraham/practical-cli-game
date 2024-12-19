109c8aa9df855e39580f0d54bbfdd2856c9a9dda
Gerardo Abraham <18366934+geoabraham@users.noreply.github.com>
2024-12-18 23:43:59 -0300

    refactor: improve wiki random page

--- src/practical_cli_game/wikipedia_random_page.py
+++ src/practical_cli_game/wikipedia_random_page.py
@@ -1,20 +1,21 @@
 import requests
-import locale
-
 from . import __version__
 
-API_URL = "https://<locale>.wikipedia.org/api/rest_v1/page/random/summary"
+API_URL = "https://{locale}.wikipedia.org/api/rest_v1/page/random/summary"
 
 
 def main(locale_code: str = None) -> tuple:
-    localed_url = (
-        API_URL.replace("<locale>", locale_code)
-        if locale_code
-        else API_URL.replace("<locale>", str(locale.getlocale()[0][0:2]))
-    )
+    locale_code = locale_code or locale.getdefaultlocale()[0]
 
-    with requests.get(localed_url) as response:
-        response.raise_for_status()
+    try:
+        response = requests.get(API_URL.format(locale=locale_code))
+
+        if not response.ok:
+            raise Exception(f"Failed to retrieve random page: {response.status_code}")
+
         data = response.json()
+        return data["title"], data["extract"]
 
-    return data["title"], data["extract"]
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None, None
